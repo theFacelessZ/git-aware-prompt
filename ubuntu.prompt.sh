b4ab1223842e78605dd21d050a7416fa356ad958
Anthony Lupow <anthony.lupow@gmail.com>
2017-06-30 14:07:42 +0300

    Added a ubuntu preset.

---
+++ ubuntu.prompt.sh
@@ -0,0 +1,3 @@
+# Include Ubuntu prompt preset.
+export PS1="\${debian_chroot:+(\$debian_chroot)}\[$txtcyn\]\u@\h\[\033[00m\]:\[\033[0;39m\]\W\[\033[01m\] \[\033[00;32m\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "  
+
